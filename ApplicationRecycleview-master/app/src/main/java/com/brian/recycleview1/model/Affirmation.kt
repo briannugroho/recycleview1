773e0b6111a87077aa823f1b7646fb6246407191
Brian wahyu nugroho <99382089+briannugroho@users.noreply.github.com>
2022-03-21 14:54:22 +0700

    Add files via upload

---
+++ ApplicationRecycleview-master/app/src/main/java/com/brian/recycleview1/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.brian.recycleview1.model
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    //Menambahkan dukungan untuk teks dan gambar di class Affirmation
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
